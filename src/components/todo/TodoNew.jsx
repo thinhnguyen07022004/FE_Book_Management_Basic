b945918977f79e1bb6c5093e04b38afea04227c1
DAD <daduc.2308@gmail.com>
2025-01-20 13:23:11 +0700

    video 33

---
+++ src/components/todo/TodoNew.jsx
@@ -0,0 +1,10 @@
+const TodoNew = () => {
+  return (
+    <div className="todo-new">
+      <input type="text" className="todo-input" placeholder="Nhập thông tin" />
+      <button className="todo-btn">Add</button>
+    </div>
+  );
+};
+
+export default TodoNew;
