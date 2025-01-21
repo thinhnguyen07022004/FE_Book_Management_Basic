872794b844a8232091060ef43ac3db057b933dc8
DAD <daduc.2308@gmail.com>
2025-01-21 16:24:33 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,12 +1,26 @@
 const TodoNew = (props) => {
-  console.log("check props", props);
+  // console.log("check props", props);
   const { addNewTodo } = props;
 
   // addNewTodo("Eric");
+  const handleClick = () => {
+    alert("Click me");
+  };
+
+  const handleOnchange = (event) => {
+    console.log(">> handleChange", event.target.value);
+  };
   return (
     <div className="todo-new">
-      <input type="text" className="todo-input" placeholder="Nhập thông tin" />
-      <button className="todo-btn">Add</button>
+      <input
+        type="text"
+        className="todo-input"
+        placeholder="Nhập thông tin"
+        onChange={handleOnchange}
+      />
+      <button className="todo-btn" onClick={handleClick}>
+        Add
+      </button>
     </div>
   );
 };
