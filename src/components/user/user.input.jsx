3d86ef706a533d76722d0a7fadf4a6d29330c572
lucidjug <tuuju2@gmail.com>
2025-03-21 09:23:59 +0700

    video 67

--- src/components/user/user.input.jsx
+++ src/components/user/user.input.jsx
@@ -1,27 +1,50 @@
 import { Button, Input } from "antd"
+import { useState } from "react"
 
 const UserInput = () => {
+    const [fullName, setFullName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [phoneNumber, setPhoneNumber] = useState("")
+    const handleClickBtn = () => {
+        console.log("check change", fullName, email, password, phoneNumber)
+    }
+
     return (
         <div className="user-form" style={{ margin: "20px 0" }}>
             <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
                 <div>
                     <span>Full Name</span>
-                    <Input />
+                    <Input
+                        value={fullName}
+                        onChange={(event) => { setFullName(event.target.value) }}
+                    />
                 </div>
                 <div>
                     <span>Email</span>
-                    <Input />
+                    <Input
+                        value={email}
+                        onChange={(event) => { setEmail(event.target.value) }}
+                    />
                 </div>
                 <div>
                     <span>Password</span>
-                    <Input.Password />
+                    <Input.Password
+                        value={password}
+                        onChange={(event) => { setPassword(event.target.value) }}
+                    />
                 </div>
                 <div>
                     <span>Phone Number</span>
-                    <Input />
+                    <Input
+                        value={phoneNumber}
+                        onChange={(event) => { setPhoneNumber(event.target.value) }}
+                    />
                 </div>
                 <div>
-                    <Button tpye="primary"> Create User </Button>
+                    <Button
+                        onClick={handleClickBtn}
+                        tpye="primary"> Create User </Button>
                 </div>
             </div>
         </div>
