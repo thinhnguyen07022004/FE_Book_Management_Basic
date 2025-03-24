f1d2b793b85cf35e727b5d667828e941de6639e0
lucidjug <tuuju2@gmail.com>
2025-03-24 15:05:38 +0700

    video 83

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,8 +1,27 @@
 import { Drawer } from 'antd';
+import { useState } from 'react';
+
+
 
 const ViewUserDetail = (props) => {
     const { dataView, setDataView, isModalViewOpen, setIsModalViewOpen } = props
 
+    const [selectedFile, setSelectedFile] = useState(null)
+    const [preview, setPreview] = useState(null)
+
+    const handleChangeImage = (event) => {
+        if (!event.target.files || event.target.files.length === 0) {
+            setSelectedFile(null)
+            setPreview(null)
+            return
+        }
+
+        const file = event.target.files[0]
+        if (file) {
+            setSelectedFile(file)
+            setPreview(URL.createObjectURL(file))
+        }
+    }
     return (
         <Drawer
             width={"40vw"}
@@ -23,14 +42,22 @@ const ViewUserDetail = (props) => {
                 <br />
                 <p>Avatar: </p>
                 <br />
-                <div>
+                <div style={{
+                    marginTop: "10px",
+                    height: "100px",
+                    width: "150px",
+                    border: "1px solid #ccc"
+                }}>
                     <img
-                        height={250}
-                        width={300}
+                        style={{
+                            height: "100%",
+                            width: "100%",
+                            objectFit: "contain"
+                        }}
                         src={`${import.meta.env.VITE_URL_BACKEND}/images/avatar/${dataView.avatar}`}
                     />
                 </div>
-                <div>
+                <div >
                     <label htmlFor='btnUpload' style={{
                         display: "block",
                         width: "fit-content",
@@ -42,8 +69,25 @@ const ViewUserDetail = (props) => {
                     }}>
                         Upload Avatar
                     </label>
-                    <input type="file" hidden id="btnUpload"></input>
+                    <input type="file" hidden id="btnUpload" onChange={(event) => { handleChangeImage(event) }}></input>
                 </div>
+                {preview && <div style={{
+                    marginTop: "10px",
+                    height: "100px",
+                    width: "150px",
+                    border: "1px solid #ccc"
+                }}>
+                    <img
+                        style={{
+                            height: "100%",
+                            width: "100%",
+                            objectFit: "contain"
+                        }}
+                        src={preview}
+                    />
+                </div>
+
+                }
             </>
                 :
                 <>
