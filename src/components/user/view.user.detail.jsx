28960926992e36fd97cdf1eb2f9bdc1948c5306f
lucidjug <tuuju2@gmail.com>
2025-03-24 14:32:32 +0700

    video 82

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -4,20 +4,46 @@ const ViewUserDetail = (props) => {
     const { dataView, setDataView, isModalViewOpen, setIsModalViewOpen } = props
 
     return (
-        <Drawer title="View Detail User"
+        <Drawer
+            width={"40vw"}
+            title="View Detail User"
             onClose={() => {
                 setDataView(null)
                 setIsModalViewOpen(false);
             }} open={isModalViewOpen}
         >
             {dataView ? <>
-                <p>Id: {dataView._id}</p>
+                <p> Id: {dataView._id}</p>
                 <br />
                 <p>Full Name: {dataView.fullName}</p>
                 <br />
                 <p>Email: {dataView.email}</p>
                 <br />
                 <p>Phone Number: {dataView.phone}</p>
+                <br />
+                <p>Avatar: </p>
+                <br />
+                <div>
+                    <img
+                        height={250}
+                        width={300}
+                        src={`${import.meta.env.VITE_URL_BACKEND}/images/avatar/${dataView.avatar}`}
+                    />
+                </div>
+                <div>
+                    <label htmlFor='btnUpload' style={{
+                        display: "block",
+                        width: "fit-content",
+                        marginTop: "15px",
+                        padding: "5px 10px",
+                        background: "orange",
+                        borderRadius: "5px",
+                        cursor: "pointer"
+                    }}>
+                        Upload Avatar
+                    </label>
+                    <input type="file" hidden id="btnUpload"></input>
+                </div>
             </>
                 :
                 <>
@@ -25,7 +51,7 @@ const ViewUserDetail = (props) => {
                 </>
             }
 
-        </Drawer>
+        </Drawer >
 
     )
 }
